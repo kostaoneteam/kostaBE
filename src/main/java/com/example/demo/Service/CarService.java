28fb6ff9d013bfd6d9925709a3130615757dfc91
OptiMasters <taeonomics@gmail.com>
2024-08-30 18:37:16 +0900

    fir

--- src/main/java/com/example/demo/Service/CarService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Service;
-
-
-public class CarService {
-
-    int a =0;
-    int b =0;
-
-}
--- src/main/java/com/example/demo/Service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.Service;
-
-public class UserService {
-
-  int a= 0;
-  int b= 0;
-
-}
