bc69e08e3786e93c3603e52be2b1fc29650b709e
tjrqh <asdww6631@gmail.com>
2024-09-02 20:38:52 +0900

    메인페이지 작업

---
+++ src/main/java/com/example/demo/application/dto/carPostDto/CarPostReadResponse.java
@@ -0,0 +1,15 @@
+package com.example.demo.application.dto.carPostDto;
+
+public class CarPostReadDto {
+
+  private String carModel;
+  private String brand;
+  private String carType;
+  private String mileage;
+  private int price;
+  private String displacement;
+  private String color;
+  private
+
+
+}
