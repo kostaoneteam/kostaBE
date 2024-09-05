6c3320ed438cdcbe8d3fad9189be3dcec6653b05
tjrqh <asdww6631@gmail.com>
2024-09-05 17:29:56 +0900

    post create기능 구현

---
+++ src/main/java/com/example/demo/application/dto/carPostDto/CarPostCreateRequest.java
@@ -0,0 +1,11 @@
+package com.example.demo.application.dto.carPostDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@AllArgsConstructor
