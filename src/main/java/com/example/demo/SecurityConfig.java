3822ec62b8b7a98af8fc56e35dac5d5bee5ca544
tjrqh <asdww6631@gmail.com>
2024-09-06 14:55:55 +0900

    w

--- src/main/java/com/example/demo/SecurityConfig.java
+++ src/main/java/com/example/demo/SecurityConfig.java
@@ -23,63 +23,6 @@ import org.springframework.web.filter.CorsFilter;
 @EnableMethodSecurity(prePostEnabled=true)
 public class SecurityConfig {
 
-<<<<<<< HEAD
-   @Bean
-   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-      http
-            .authorizeHttpRequests(authorizeHttpRequests ->
-                  authorizeHttpRequests
-                        .requestMatchers("/swagger-ui.html", "/v3/api-docs/**", "/docs/**").permitAll()
-                        .requestMatchers("/**").permitAll()
-            )
-            .csrf(csrf ->
-                  csrf
-                        //.ignoringRequestMatchers("/h2-console/**")
-                        .ignoringRequestMatchers("/**")
-            )
-            .headers(headers ->
-                  headers.addHeaderWriter(new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN))
-            )
-            .formLogin(formLogin ->
-                  formLogin
-                        .loginPage("/user/login")
-                        .defaultSuccessUrl("/")
-            )
-            .logout(logout ->
-                  logout
-                        .logoutRequestMatcher(new AntPathRequestMatcher("/user/logout"))
-                        .logoutSuccessUrl("/")
-                        .invalidateHttpSession(true)
-            )
-            .cors(Customizer.withDefaults());
-      return http.build();
-   }
-
-   // CORS 설정을 위한 Bean 생성
-   @Bean
-   public CorsFilter corsFilter() {
-      UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-      CorsConfiguration config = new CorsConfiguration();
-
-      config.setAllowCredentials(true);
-      config.addAllowedOrigin("http://127.0.0.1:5500"); // 모든 출처 허용 (프로덕션 환경에서는 특정 도메인만 허용하는 것이 좋습니다)
-      config.addAllowedHeader("*"); // 모든 헤더 허용
-      config.addAllowedMethod("*"); // 모든 HTTP 메서드 허용 (GET, POST, PUT, DELETE 등)
-
-      source.registerCorsConfiguration("/**", config);
-      return new CorsFilter(source);
-   }
-
-   @Bean
-   public PasswordEncoder passwordEncoder() {
-      return new BCryptPasswordEncoder();
-   }
-
-   @Bean
-   public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-      return authenticationConfiguration.getAuthenticationManager();
-   }
-=======
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
@@ -135,5 +78,4 @@ public class SecurityConfig {
 	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
 		return authenticationConfiguration.getAuthenticationManager();
 	}
->>>>>>> 87890a45762d8a607859a928602af5b570703cb1
 }
